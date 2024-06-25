7f6f9d547fac7e503a8e4976a7376c577ae86b02
raman.hrynkevich <raman.hrynkevich@gitlab.azati.by>
2024-06-25 10:26:25 +0300

    Added last message check, to protect against spam if a small delay between requests is set.

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import asyncio
 from discord.ext import commands
 from datetime import datetime, timezone
 
-
 # Discord bot keys
 TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
 CHANNEL_ID = YOUR_DISCORD_CHANNEL_ID
@@ -16,11 +15,16 @@ SYMBOL = 'SOLUSDT'
 TIMEFRAME = '60'  # in minutes
 
 # Delay parameter
-STARTUP_FREQUENCY = 3600  # in seconds
+STARTUP_FREQUENCY = 1  # in seconds
+
+# Variables for tracking the last message
+last_message = None
+last_message_time = None
 
 # Initializing the bot
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 # Function for obtaining K-line data
 def get_klines(symbol, interval):
@@ -43,6 +47,7 @@ def calculate_rsi(data):
 
 # Function to check RSI and send a message
 async def check_rsi():
+    global last_message, last_message_time
     await bot.wait_until_ready()
     channel = bot.get_channel(CHANNEL_ID)
     while not bot.is_closed():
@@ -51,12 +56,18 @@ async def check_rsi():
         last_timestamp = data['timestamp'].iloc[-1]
         current_time = datetime.now(timezone.utc)
         time_diff = (current_time - last_timestamp).total_seconds()
-        message = f'The RSI for {SYMBOL} is {rsi:.2f}.'
+
         if time_diff <= 10:
+            message = None
             if rsi > 70:
-                await channel.send(message)
+                message = f'The RSI for {SYMBOL} is {rsi:.2f}.'
             elif rsi < 30:
+                message = f'The RSI for {SYMBOL} is {rsi:.2f}.'
+
+            if message and (message != last_message or (current_time - last_message_time).total_seconds() > 10):
                 await channel.send(message)
+                last_message = message
+                last_message_time = current_time
 
         await asyncio.sleep(STARTUP_FREQUENCY)
 
@@ -64,7 +75,7 @@ async def check_rsi():
 # Starting a task when the bot is ready
 @bot.event
 async def on_ready():
-    bot.loop.create_task(check_rsi())
+    await bot.loop.create_task(check_rsi())
     print(f'Bot {bot.user} is up and running and ready to go!')
 
 
